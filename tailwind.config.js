f08d47766407589f38ab66869d46e428f2af7764
Chandru <chandru89new@gmail.com>
2023-06-05 09:09:18 +0530

    Init commit.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./tmp/**/*.html", "./templates/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ["Sora", "sans-serif"],
+      },
+      fontSize: {
+        xxs: "0.5rem",
+      },
+    },
+  },
+  plugins: [],
+};
